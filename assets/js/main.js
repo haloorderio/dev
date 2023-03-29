27f59bbdf440cdc36f25d45cc4b5576ce965468f
degiam <degiam@ymail.com>
2023-03-29 19:16:05 +0700

    Modify product detail

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,4 @@
  * Designed by Degiam [https://degiam.github.io]
  * Copyright (c) 2023
  */
-function test(selector) {
-	document.querySelectorAll('.bg-white').forEach(item => {
-		console.log(item.innerText)
-	})
-}
-test('.bg-white')
+
