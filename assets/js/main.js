eadc98eea8db0078daca1c38ad5b2a4d05c64204
degiam <degiam@ymail.com>
2023-03-30 20:00:24 +0700

    Fixing currency with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,9 @@
  * Copyright (c) 2023
  */
 
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ".")
+}
+document.querySelectorAll('.money').forEach(item => {
+	item.innerHTML = numberWithCommas(item.innerHTML)
+})
