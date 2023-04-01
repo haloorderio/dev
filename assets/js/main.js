00c6fa66c84ff2d178116e4dafe1cf52be925a06
degiam <degiam@ymail.com>
2023-04-01 10:06:20 +0700

    Fixing currency

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,24 @@
  * Copyright (c) 2023
  */
 
-function numberWithCommas(x) {
-	return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ".")
-}
-document.querySelectorAll('.currency').forEach(item => {
-	let currency = numberWithCommas(item.innerHTML)
-	item.innerHTML = currency
-})
+// Product Slider
+	for (let i=0; i<document.getElementsByClassName('splide').length; i++) {
+		new Splide('.splide', {
+			pagination: false,
+		}).mount()
+	}
 
-for (let i=0; i<document.getElementsByClassName('splide').length; i++) {
-	new Splide('.splide', {
-		pagination: false,
-	}).mount()
-}
+// Product Currency
+	// function numberWithCommas(x) {
+	// 	return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ".")
+	// }
+
+	function numberWithCommas(num) {
+	  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g,'.')
+	}
+
+	document.querySelectorAll('.currency').forEach(item => {
+		let currency = numberWithCommas(item.innerHTML)
+		item.innerHTML = currency
+	})
+
